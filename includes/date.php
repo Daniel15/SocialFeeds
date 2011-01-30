9f08b3459e443ebd2735b3d637fb07f66e79b1b9
Daniel15 <daniel@dan.cx>
2011-01-30 18:09:11 +1100

    Add license to code

--- includes/date.php
+++ includes/date.php
@@ -1,4 +1,24 @@
 <?php
+/*
+ * Social Feeds
+ * Copyright (C) 2011, Daniel Lo Nigro (Daniel15) <daniel at dan.cx>
+ * 
+ * This file is part of Social Feeds.
+ * 
+ * Social Feeds is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Social Feeds is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Social Feeds.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 /**
  * Various date functions
  */
