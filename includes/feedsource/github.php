461cd7b97956504473b3e4d1f04351fabc53efdd
Daniel Lo Nigro <daniel@dan.cx>
2013-02-21 23:04:11 +1100

    Display Github feed and call Github API instead of using hardcoded data. Closes #2

--- includes/feedsource/github.php
+++ includes/feedsource/github.php
@@ -25,26 +25,60 @@
  */
 class FeedSource_Github extends FeedSource
 {
-	// TOOD: Ensure this sends a User-Agent
+	/**
+	 * URL to the user events feed from GitHub's API
+	 */
 	const API_URL = 'https://api.github.com/users/%s/events';
+	/**
+	 * Number of items per page
+	 */
+	const ITEMS_PER_PAGE = 30;
+	/**
+	 * Maximum page number for the API endpoint
+	 */
+	const MAX_PAGE = 10;
 
 	/**
 	 * Update the feed with all the latest items
 	 */
 	public function doUpdate()
 	{
-		//$url = sprintf(self::API_URL, $this->username);
-		$url = '/var/www/socialfeeds/github2.json';
-
-		$data = json_decode(file_get_contents($url));
 		$this->latest_id = (int)$this->latest_id;
+		$this->prev_latest_id = $this->latest_id;
+
+		$page = 1;
+		do
+		{
+			$new_items = $this->doUpdatePage($page);
+			$page++;
+		}
+		// Keep iterating while we're adding a full page of items. Since we added a full page, there's a chance there
+		// could be more new items on the next page.
+		while ($new_items == static::ITEMS_PER_PAGE && $page <= static::MAX_PAGE);
+	}
+
+	private function doUpdatePage($page)
+	{
+		$new_items = 0;
+
+		$url = sprintf(self::API_URL, $this->username) . '?page=' . $page;
+		$stream = stream_context_create([
+			'http' => [
+				'method' => 'GET',
+				'header' => "User-Agent: Daniel15-SocialFeeds/1.0 (socialfeeds@dan.cx; http://github.com/Daniel15/SocialFeeds)"
+			]
+		]);
+		$data = json_decode(file_get_contents($url, false, $stream));
 
 		foreach ($data as $item)
 		{
 			$time = strtotime($item->created_at);
+
 			// Skip it if it's older than the latest item
-			if ($time <= $this->latest_id)
+			if ($time <= $this->prev_latest_id)
 				continue;
+
+			$new_items++;
 
 			$processMethod = 'process' . $item->type;
 			// Ensure we actually know what this event type is
@@ -76,12 +110,25 @@ class FeedSource_Github extends FeedSource
 			$this->saveToDB($item->id, $time, $text, null, $url, $extra_data, $visible);
 			$this->latest_id = max($this->latest_id, $time);
 		}
+
+		return $new_items;
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	private function processCreateEvent($item, &$url, &$text, &$visible, &$extra_data)
 	{
-		$text = $item->payload->ref;
+		if ($item->payload->ref_type == 'repository')
+		{
+			$text = $item->repo->name;
+			$url = static::getRepoUrlFromApiUrl($item->repo->url);
+		}
+		else
+		{
+			$text = $item->payload->ref;
+			// Github doesn't supply the branch or tag URL in the API :(
+			$url = static::getRepoUrlFromApiUrl($item->repo->url) . '/tree/' . $text;
+		}
+
 		$extra_data += [
 			'ref_type' => $item->payload->ref_type,
 		];
@@ -186,12 +233,78 @@ class FeedSource_Github extends FeedSource
 	 */
 	public function loadFromDB($row)
 	{
-		throw new Exception('TODO');
-		// TODO: Implement loadFromDB() method.
+		$link = '<a href="' . $row->url . '" rel="nofollow">' . $row->text . '</a>';
+		$description = '';
+
+		switch ($row->extra_data['type'])
+		{
+			case 'CreateEvent':
+				if ($row->extra_data['ref_type'] == 'branch')
+				{
+					$text = 'Created new branch ' . $link . ' on <a href="' . $row->extra_data['repo']['url'] . '" rel="nofollow">' . $row->extra_data['repo']['name'] . '</a>';
+				}
+				elseif ($row->extra_data['ref_type'] == 'branch')
+				{
+					$text = 'Created new tag ' . $link . ' for <a href="' . $row->extra_data['repo']['url'] . '" rel="nofollow">' . $row->extra_data['repo']['name'] . '</a>';
+				}
+				elseif ($row->extra_data['ref_type'] == 'repository')
+				{
+					$text = 'Created new repository ' . $link;
+				}
+				break;
+			case 'FollowEvent':
+				$text = 'Started following ' . $link;
+				break;
+			case 'ForkEvent':
+				$text = 'Forked ' . $row->extra_data['repo']['name'] . ' to ' . $link;
+				break;
+			case 'GistEvent':
+				$text = ucfirst($row->extra_data['action']) . 'd a Gist: ' . $link;
+				break;
+			case 'IssuesEvent':
+				$text = ucfirst($row->extra_data['action']) . ' issue ' . $link;
+				break;
+			case 'PullRequestEvent':
+				$text = ucfirst($row->extra_data['state']) . ' pull request ' . $link . ' for <a href="' . $row->extra_data['repo']['url'] . '" rel="nofollow">' . $row->extra_data['repo']['name'] . '</a>';
+				break;
+			case 'PushEvent':
+				if (count($row->extra_data['commits']) == 1)
+				{
+					$text = 'Pushed to ' . $row->extra_data['repo']['name'] . ': <a href="' . $row->extra_data['commits'][0]['url'] . '" rel="nofollow">' . $row->extra_data['commits'][0]['message'] . '</a>';
+				}
+				else
+				{
+					$text = 'Pushed ' . count($row->extra_data['commits']) . ' commits to <a href="' . $row->extra_data['repo']['url'] . '" rel="nofollow">' . $row->extra_data['repo']['name'] . '</a>';
+					$description = '<ul>';
+					foreach ($row->extra_data['commits'] as $commit)
+					{
+						$description .= '<li><a href="' . $commit['url'] . '" rel="nofollow">' . $commit['message'] . '</a></li>';
+					}
+					$description .= '</ul>';
+				}
+				break;
+			case 'WatchEvent':
+				$text = 'Started watching ' . $link;
+				break;
+
+			// Any unrecognised events. Should never happen as unrecognised events are not saved to the DB
+			default:
+				$text = $row->extra_data['type'] . ': ' . $row->text;
+				break;
+		}
+
+		return (object)array(
+			'id' => $row->id,
+			'text' => $text,
+			'description' => $description,
+			'url' => $row->url,
+			'date' => $row->date,
+			'type' => 'github',
+		);
 	}
 
 	/**
-	 * A bit of a hack to convert a Github repository API URL (eg. http://api.github.com/repos/Daniel15/SocialFeeds) intp
+	 * A bit of a hack to convert a Github repository API URL (eg. http://api.github.com/repos/Daniel15/SocialFeeds) into
 	 * the public website URL.
 	 *
 	 * @static
